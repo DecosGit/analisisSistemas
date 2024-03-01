2e6578d97052832c607cc533f5e449e92a624d91
brianmaldonado17 <brian.maldonado17@gmail.com>
2024-02-29 21:51:33 -0600

    Correcciones y método post createUser

--- router.js
+++ router.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const query = require('./database/sqlserver')
+const Swal = require('sweetalert2');
+
 
 router.get('/', (req, res) => {
     res.render('login');
@@ -40,5 +42,45 @@ router.post('./validateUser', (req, res) => {
     console.log('logueado')
     res.send('Logueado');
 })
+
+router.post('/createUser', (req, res) => {
+    // Llamada a la función registrarUsuario()
+    async function registrarUsuario() {
+        var cui = document.getElementById("uiCui").value;
+        var contrasenia = document.getElementById("uiContrasenia").value;
+    
+        try {
+            // Ejecutar el procedimiento almacenado
+            const result = await request
+                .input('id_usuario', mssql.NVarChar(50), cui)
+                .input('password', mssql.NVarChar(25), contrasenia)
+                .input('estado', mssql.TinyInt, 1)
+                .input('accion', mssql.Char(1), 'I')
+                .execute('sp_acciones_usuario');
+            
+            // Mostrar mensaje de éxito
+            Swal.fire({
+                icon: 'success',
+                title: 'Éxito',
+                text: 'Usuario registrado correctamente',
+            });
+        
+            // Limpiar formulario
+            document.getElementById("formularioUsuario").reset();
+            res.redirect('/dashboard');
+        
+        } catch (error) {
+            console.error(error);
+            // Mostrar mensaje de error
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'Ha ocurrido un error al registrar el usuario',
+            });
+        }
+    }
+    // Redirigir a otra página después de mostrar el mensaje
+    
+});
 
 module.exports = router;
