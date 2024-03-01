9472cb6825ed16293576e8b7c2ba1d9d9b4817ba
brianmaldonado17 <brian.maldonado17@gmail.com>
2024-03-01 11:25:00 -0600

    Cambios finales para el funcionamiento del formulario para crear usuarios

--- controller/userData.js
+++ controller/userData.js
@@ -1,4 +1,5 @@
 const { getConection } = require('../database/conexionSQLServer')
+const mssql = require('mssql');
 
 exports.findUser = async (req, res) => {
 
@@ -37,10 +38,31 @@ exports.createUser = async (req, res) =>
 {
     try 
     {
+        let correo = req.body.correo
         let cui = req.body.cui
         let contrasenia = req.body.contrasenia
-        console.log(cui + ' ' + contrasenia);
+        let primerNombre = req.body.primerNombre
+        let segundoNombre = req.body.segundoNombre || ""
+        let otrosNombres = req.body.tercerNombre || ""
+        let primerApellido = req.body.primerApellido
+        let segundoApellido = req.body.segundoApellido || ""
+        let apellidoCasada = req.body.apellidoCasada || ""
 
+        let nombreCompleto = `${primerNombre} ${segundoNombre} ${otrosNombres} ${primerApellido} ${segundoApellido}`
+
+        if (apellidoCasada) 
+        {
+            nombreCompleto += ` de ${apellidoCasada}`
+        }
+        // Eliminar espacios adicionales
+        nombreCompleto = nombreCompleto.replace(/\s+/g, ' ').trim()
+        
+        let hoy = new Date()
+        let dia = hoy.getDate()
+        let mes = hoy.getMonth() + 1 // Los meses van de 0 a 11, por eso se suma 1
+        let anio = hoy.getFullYear()
+        let fechaCorta = `${dia}/${mes}/${anio}`
+         
         const pool = await getConection();
         const request = pool.request();
 
@@ -50,9 +72,20 @@ exports.createUser = async (req, res) =>
             .input('password', mssql.NVarChar(25), contrasenia)
             .input('estado', mssql.TinyInt, 1)
             .input('accion', mssql.Char(1), 'I')
+            .input('nombre_reporte', mssql.VarChar(100), nombreCompleto)
+            .input('email', mssql.VarChar(100), correo)
+            .input('usuario_ing', mssql.VarChar(50), 'admin')
+            .input('fecha_ing', mssql.SmallDateTime, fechaCorta)
+            .input('usuario_act', mssql.VarChar, null)
+            .input('fecha_act', mssql.SmallDateTime, null)
+            .input('nombre_completo', mssql.VarChar(mssql.MAX), nombreCompleto)
+            .input('cod_unidad', mssql.VarChar(6), null)
+            .input('cod_puesto', mssql.SmallInt, null)
+            .input('cod_rol_usuario', mssql.VarChar(10), 'contr')
+            .input('cui', mssql.VarChar(15), cui)
             .execute('sp_acciones_usuario');
         
-            res.render('login', { alertMessage: 'Usuario creado exitosamente, tu usuario es: ' + cui});
+            res.render('login', { alertMessage: 'Tu usuario es: ' + cui});
 
     } catch (error) 
     {
